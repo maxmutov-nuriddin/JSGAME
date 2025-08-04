053a56041e8a05b17dc9965c387a8c37632de1e4
Maxmutov Nuriddin <mahmudovnuriddin35@gmail.com>
2025-08-05 00:47:33 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+let sBtn = document.querySelector("#start_btn");
+let fData = document.querySelector("#input_data");
+let cBtn = document.querySelector("#check_btn");
+let result = document.querySelector("#result");
+let life = document.querySelector("#life");
+let sms = document.querySelector("#sms");
+let game = document.querySelector("#game");
+let oBtn = document.querySelector("#okey_btn");
+let rGame = document.querySelector("#result_game");
+
+let rNumb;
+let lNumb;
+
+life.textContent = "";
+
+result.textContent = "Нажми на старт";
+
+sBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  rNumb = Math.floor(Math.random() * 50);
+
+  lNumb = Math.ceil(Math.random() * 5);
+
+  life.textContent = lNumb--;
+  result.textContent = "Я готов";
+
+  console.log(rNumb);
+
+  cBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (rNumb == undefined) {
+      game.classList.add("visually-hidden");
+    }
+    if (rNumb < fData.value) {
+      life.textContent = lNumb--;
+      result.textContent = "Слишком много!";
+      fData.value = "";
+    } else if (rNumb > fData.value) {
+      life.textContent = lNumb--;
+      fData.value = "";
+      result.textContent = "Слишком мало!";
+    } else if (rNumb == fData.value) {
+      life.textContent = "";
+      rGame.textContent = "Поздравляю ты угодал 😊!";
+      fData.value = "";
+      game.classList.add("visually-hidden");
+      sms.classList.remove("visually-hidden");
+    }
+
+    if (lNumb == -1) {
+      game.classList.add("visually-hidden");
+      sms.classList.remove("visually-hidden");
+      rGame.textContent = "Tы проиграл 😔!";
+    }
+  });
+});
+
+cBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (rNumb == undefined) {
+    game.classList.add("visually-hidden");
+    sms.classList.remove("visually-hidden");
+  }
+});
+
+oBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  location.reload();
+  game.classList.remove("visually-hidden");
+  sms.classList.add("visually-hidden");
+});
